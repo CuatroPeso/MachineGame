f5f08ec1a57c16f9968b8684444e7887ba4dc145
Luciano Tejeda <134396567+CuatroPeso@users.noreply.github.com>
2025-11-01 22:23:33 -0300

    Delete machinegame directory

--- machinegame/src/machinegame/settings.py
+++
@@ -1,7 +0,0 @@
-"""Kedro project settings.
-
-https://docs.kedro.org/en/stable/kedro_project_setup/settings.html
-"""
-
-# No usamos hooks (el template traía SparkHooks, lo desactivamos)
-HOOKS = ()
